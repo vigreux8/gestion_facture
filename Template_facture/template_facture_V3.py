41708a044248d5c750130bcf3b6985032c774cc7
vigreux8 <vigreux8@hotmail.fr>
2023-08-24 10:59:02 +0200

    model OP

--- Template_facture/template_facture_V3.py
+++ Template_facture/template_facture_V3.py
@@ -4,19 +4,20 @@ from fonctions.fonction_models_commun import facture_fonction_commun
 #date : {{date}}
 
 class ModelFacture(facture_fonction_commun):
-    def __init__(self,path_facture_amazon_prime) -> None:
-        super().__init__(path_facture_amazon_prime)
+    def __init__(self,path_facture) -> None:
+        super().__init__(path_facture)
         self.provenance = "nom_provenance"
         print(f"instance : {self.provenance} active")
-        self.facture["path"] = path_facture_amazon_prime
-        self.facture["name"] = os.path.basename(path_facture_amazon_prime)
-        self.provenance = "numero unique"
+        self.path = path_facture
+        self.nom = os.path.basename(path_facture)
+        self.id_provenance = "numero unique"
+        self.url_facture = None
         
         #1
 
         
         self.get_contenue_pdf()
-        if  self.pattern_provenance in self.contenue_pdf_byte:
+        if  self.id_provenance in self.contenue_pdf_byte:
             self.run_programme_model()
         else:
             print(f"se n'ai pas une facture {self.provenance}")
--- model_facture/model_adobes.py
+++ model_facture/model_adobes.py
@@ -4,12 +4,12 @@ from fonctions.fonction_models_commun import facture_fonction_commun
 #date : {{date}}
 
 class ModelFacture(facture_fonction_commun):
-    def __init__(self,path_facture_amazon_prime) -> None:
-        super().__init__(path_facture_amazon_prime)
+    def __init__(self,path_facture) -> None:
+        super().__init__(path_facture)
         self.provenance = "adobe"
         print(f"instance : {self.provenance} active")
-        self.path = path_facture_amazon_prime
-        self.nom = os.path.basename(path_facture_amazon_prime)
+        self.path = path_facture
+        self.nom = os.path.basename(path_facture)
         self.id_provenance = "IE6364992H"
         self.url_facture = None
         
